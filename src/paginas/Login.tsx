1b880dc8bb4670a7f5c4ae74020a2bd665264948
Daniel Montiel <danielponce1397@gmail.com>
2024-10-15 11:05:12 -0600

    Se agrego email de prueba de inicio de sesion

--- src/paginas/Login.tsx
+++ src/paginas/Login.tsx
@@ -43,7 +43,6 @@ export default function Login() {
         email: veterinario.email,
         password: veterinario.password
       });
-
       localStorage.setItem('token',data.token)
       setVeterinario({
         email: '',
@@ -118,6 +117,13 @@ export default function Login() {
           <input type="submit" className="bg-indigo-700 w-full py-3 rounded-xl text-white uppercase font-bold hover:cursor-pointer hover:bg-indigo-800 md:w-auto px-10" value="Iniciar Sesión"/>
           
         </form>
+        <div className="my-5">
+          <label className="text-center uppercase text-gray-600 block text-l font-bold">Prueba Inicio de Sesión</label>
+          <div className="flex justify-around my-3">
+            <label className="uppercase text-gray-600 block text-sm font-bold">Email: <span className="font-normal lowercase">daniel@gmail.com</span></label>
+            <label className="uppercase text-gray-600 block text-sm font-bold">Password: <span className="font-normal lowercase">123456</span></label>
+          </div>
+        </div>
         <nav className="mt-10 lg:flex lg:justify-between">
           <Link className="block text-center my-5 text-gray-500" to="/registrar">¿No tienes una cuenta? Registrate</Link>
           <Link className="block text-center my-5 text-gray-500" to="/olvide-password">Olvide mi password</Link>
